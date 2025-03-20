5fabf0f84a511ded9858cabb06eea594826bc103
Edgar J Montoya Alvarez <edgarjmontoya@hotmail.com>
2025-03-19 22:56:57 -0500

    Versión 2

--- Prueba2.py
+++ Prueba2.py
@@ -1,2 +1,10 @@
-print("Hola Mundo")
-nombre = input("Ingrese su nombre ")
+#Escribe un algoritmo que convierta una temperatura dada en grados Celsius a grados Fahrenheit.
+
+#Obtenemos la temperatura en celsius
+Temperatura = int(input("Por favor ingrese la temperatura en celsius para convertirla en fahrenheit"))
+
+#Convertimos la temperatura
+Conversion = (Temperatura * (9/5)) + 32
+
+#Imprimimos la conversiónext
+print("\nLa temperatura en fahenheit es: ", Conversion)
